cdd37c636398ce829614bc7b53becb693b48a637
Daniil Molotkov <molotkov.58@ya.ru>
2022-03-05 14:55:48 +0700

    Добавьте файлы проекта.

---
+++ TableCommandGenerator_WPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TableCommandGenerator_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (nameTable.Text != "" && indexArray.Text != "" && indexId.Text != "")
+            {
+                String answer;
+                answer = $"=ВЫБОР({indexId.Text};";
+                int indexStart = int.Parse(indexArray.Text.Split(':')[0].Split(' ')[1]);
+                for (int i = indexStart; i <= int.Parse(namberOfNotes.Text) + indexStart; i++)
+                {
+                    if (i != int.Parse(namberOfNotes.Text) + indexStart && !nameTable.Text.Contains(' '))
+                        answer += $"TEXTJOIN(\" \"; ИСТИНА; {nameTable.Text}!{indexArray.Text.Split(':')[0].Split(' ')[0]}{i}:{indexArray.Text.Split(':')[1].Split(' ')[0]}{i});";
+                    if (i == int.Parse(namberOfNotes.Text) + indexStart && !nameTable.Text.Contains(' '))
+                        answer += $"TEXTJOIN(\" \"; ИСТИНА;{nameTable.Text}!{indexArray.Text.Split(':')[0].Split(' ')[0]}{i}:{indexArray.Text.Split(':')[1].Split(' ')[0]}{i}))";
+                    if (i != int.Parse(namberOfNotes.Text) + indexStart && nameTable.Text.Contains(' '))
+                        answer += $"TEXTJOIN(\" \"; ИСТИНА; '{nameTable.Text}'!{indexArray.Text.Split(':')[0].Split(' ')[0]}{i}:{indexArray.Text.Split(':')[1].Split(' ')[0]}{i});";
+                    if (i == int.Parse(namberOfNotes.Text) + indexStart && nameTable.Text.Contains(' '))
+                        answer += $"TEXTJOIN(\" \"; ИСТИНА;'{nameTable.Text}'!{indexArray.Text.Split(':')[0].Split(' ')[0]}{i}:{indexArray.Text.Split(':')[1].Split(' ')[0]}{i}))";
+
+                }
+                OutputTextBox.AppendText(answer);
+
+            }
+        }
+    }
+}
